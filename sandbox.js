a7a12c7a3276d5d6f16712cad3b67c4e917def8f
masterseeker <juldan01@googlemail.com>
2020-10-21 22:30:50 +0100

    first commit

---
+++ sandbox.js
@@ -0,0 +1,9 @@
+console.log("Hello World");
+
+bodyEl = document.querySelector("body");
+bodyEl.style.backgroundColor = "cyan";
+bodyEl.style.color = "#0000ff";
+
+newTag = document.createElement("meter");
+bodyEl.appendChild("newTag");
+
